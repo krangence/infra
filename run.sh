9fed542c9f8421844a4206ea30eca01af1fb20db
Chip Wolf <hello@chipwolf.uk>
2020-04-26 13:26:23 +0100

    :sparkles: added flux deployment during initial cluster creation

--- run.sh
+++ run.sh
@@ -9,12 +9,16 @@ tail -f output.txt &
 if [[ "$(eksctl create cluster -f cluster.yaml | tee output.txt)" = *AlreadyExistsException* ]]; then
     set -e
     eksctl update cluster -f cluster.yaml --approve
-		eksctl create nodegroup -f cluster.yaml
-		eksctl delete nodegroup -f cluster.yaml --only-missing --approve
-		eksctl utils update-kube-proxy -f cluster.yaml --approve
-		eksctl utils update-aws-node -f cluster.yaml --approve
-		eksctl utils update-coredns -f cluster.yaml --approve
-		eksctl create iamidentitymapping -f cluster.yaml --arn arn:aws:iam::521397258504:role/admin --group system:masters --username admin
+	eksctl create nodegroup -f cluster.yaml
+	eksctl delete nodegroup -f cluster.yaml --only-missing --approve
+	eksctl utils update-kube-proxy -f cluster.yaml --approve
+	eksctl utils update-aws-node -f cluster.yaml --approve
+	eksctl utils update-coredns -f cluster.yaml --approve
+	eksctl create iamidentitymapping -f cluster.yaml --arn arn:aws:iam::521397258504:role/admin --group system:masters --username admin
+else
+	set -e
+	EKSCTL_EXPERIMENTAL=true
+	eksctl enable repo -f cluster.yaml --git-url=git@github.com:HRZNStudio/playhrzn-k8s.git --git-email=accounts@hrznstudio.com
 fi
 
 exit $?
